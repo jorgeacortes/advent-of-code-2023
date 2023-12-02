786b7cafebc5de5de2434c84311d05c3644f2bbb
jorgeacortes <jorgeacortes@users.noreply.github.com>
2023-12-02 15:21:32 +0100

    day2

--- lib/02_cube_conundrum/cube_conundrum.ex
+++ lib/02_cube_conundrum/cube_conundrum.ex
@@ -2,7 +2,89 @@ defmodule AdventOfCode2023.CubeConundrum do
   @moduledoc false
 
   def read_file(file_path, rules) do
-    {:error, rules, 0}
+    case File.read(file_path) do
+      {:ok, content} ->
+        games_record =
+        read_games(content)
+        |> Enum.map(fn {id, cube_string} -> {id, read_cube_string(cube_string)} end)
+
+        result =
+          games_record
+          |> Enum.map(fn {id, game} -> {id, sum_cubes_by_color_and_play(game)}end)
+          |> validate_games(rules)
+          |> Enum.filter(fn {_, possible} -> possible end)
+          |> Enum.map(fn {game_number, true} -> game_number end)
+
+        sum = Enum.reduce(result, 0,  fn game, acc -> acc + game end)
+
+      {:ok, result, sum}
+
+      {:error, reason} ->
+        IO.puts("Error reading the file: #{reason}")
+        :error
+    end
+  end
+
+  defp read_games(doc) do
+    doc
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> String.split(line, ": ", trim: true) end)
+    |> Enum.map(fn [game | [cubes | _]] -> {get_game_number(game), cubes} end)
+  end
+
+  defp get_game_number(s) do
+    s
+    |> String.replace("Game ", "")
+    |> String.to_integer()
+  end
+
+  defp read_cube_string(cubes) do
+      cubes
+      |> String.split("; ", trim: true)
+      |> Enum.map(fn play -> String.split(play, ", ", trim: true) end)
+      |> Enum.map(fn play -> map_cubes(play) end)
+  end
+
+  defp map_cubes(cubes) do
+    cubes
+      |> Enum.map(fn x -> String.split(x, " ", trim: true) end)
+      |> Enum.map(fn [num | [color]] -> {String.to_integer(num), color} end)
+
+  end
+
+  defp sum_play_cubes_by_color(play) do
+    play
+      |> Enum.reduce(%{}, fn {count, id}, acc ->
+        atom_id = String.to_atom(id)
+        Map.update(acc, atom_id, count, fn prev -> prev + count end)
+      end)
+  end
+
+  defp sum_cubes_by_color_and_play(game_plays) do
+    game_plays
+      |> Enum.map(fn play -> sum_play_cubes_by_color(play) end)
+  end
+
+  defp validate_games(games, rules) do
+    games
+      |> Enum.map(fn game -> validate_game(game, rules) end)
+  end
+
+  defp validate_game({id, plays}, rules) do
+    possible =
+      plays
+        |> Enum.map(fn play -> validate_play(play, rules) end)
+        |> Enum.all?(fn possible -> possible == true end)
+    {id, possible}
+  end
+
+  defp validate_play(cubes, rules) do
+      cubes
+        |> Map.to_list()
+        |> Enum.all?(
+          fn {cube_name, value} ->
+            value <= Map.get(rules, cube_name)
+          end)
   end
 
 end
--- test/02_cube_conundrum/cube_conundrum_test.exs
+++ test/02_cube_conundrum/cube_conundrum_test.exs
@@ -1,13 +1,37 @@
 defmodule CubeConundrumTest do
   use ExUnit.Case
   alias AdventOfCode2023.CubeConundrum
-
   doctest CubeConundrum
 
-  test "CubeConundrum sample game is processed correctly" do
-    rules = ["12 red", "13 green", "14 blue"]
-    games_possible = [1, 2, 5]
-    games_sum = 8
-    assert {:ok, games_possible, games_sum} == CubeConundrum.read_file("./test/02_cube_conundrum/data/document.txt", rules)
-  end
+  ExUnit.Case.register_attribute(__MODULE__, :data)
+
+  @test_cases [
+    %{
+      rules: %{red: 12, green: 13, blue: 14},
+      games_possible: [1, 2, 5],
+    },
+    %{
+      rules: %{red: 12, green: 13, blue: 0},
+      games_possible: [],
+    },
+    %{
+      rules: %{red: 20, green: 20, blue: 20},
+      games_possible: [1,2,3,4,5],
+    },
+    %{
+      rules: %{red: 30, green: 30, blue: 30},
+      games_possible: [1,2,3,4,5],
+    },
+  ]
+
+  Enum.with_index(@test_cases, fn data, idx ->
+    @data data
+    test "CubeConundrum sample game is processed correctly: case #{idx}", context do
+      %{rules: rules, games_possible: games} = context.registered.data
+      sum = Enum.sum(games)
+      assert {:ok, games, sum} == CubeConundrum.read_file(
+              "./test/02_cube_conundrum/data/document.txt", rules)
+    end
+  end)
+
 end
